fc757b2d253f2fc373bb358f9cff345c049e8294
Praveen Nagaraj <praveennagaraj597@gmail.com>
2022-04-18 23:52:16 +0530

    admin panel init

---
+++ admin/src/App.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import Home from './components/Home/index.vue';
+import Footer from './layout/footer/index.vue';
+import Header from './layout/header/index.vue';
+</script>
+
+<template>
+  <Header />
+  <Home />
+  <Footer />
+</template>
